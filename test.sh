1a83ce272183ef8b6a61ae28df4118781736e363
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2015-12-17 16:05:00 +0100

    🐛 ignore generated file
    The *.egg-info was generated during the build process by pip and should
    have been ignored.

--- test.sh
+++ test.sh
@@ -12,5 +12,12 @@ trap cleanup EXIT
 /bin/bash ${0%/*}/build.sh
 (
    cd cookiecutter_template
+
+   if [[ -n $(git status -s) ]];
+   then
+	   git status
+	   echo "Uncommitted changes detected"
+	   exit 1
+   fi
    tox
 )
